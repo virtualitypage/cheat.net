17ffb6f5c0e8ac2055bf047d339ac27111d0b891
virtualitypage <121924587+virtualitypage@users.noreply.github.com>
2023-06-07 21:52:26 +0900

    add to jq command

--- laboratory/shell/wsl_CentOS8_setting.sh
+++ laboratory/shell/wsl_CentOS8_setting.sh
@@ -5,5 +5,5 @@ sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 cd
 yum groupinstall -y "Development Tools"
-yum install -y bc which sudo wget make
+yum install -y bc which sudo wget make jq
 echo "attention: bc which sudo wget make installed!"
