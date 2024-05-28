0387ab28b1a0fc6eaddbaacad13e73c13ac8a800
virtualitypage <ngd_apm@icloud.com>
2024-05-28 10:11:38 +0900

    modified

--- laboratory/batchfile/lobotomy_volumes.bat
+++ laboratory/batchfile/lobotomy_volumes.bat
@@ -2,8 +2,8 @@
 rem robocopyコマンドでファイルの移動を実行するバッチファイル
 
 rem 移動元ディレクトリの中にあるファイルが対象
-set src_dir=D:\Untitled\DCIM\100MEDIA
-set dst_dir=E:\HDCL-UT
-set log_file=E:\robocopy_log.txt
+set src_dir=E:\DCIM\100MEDIA
+set dst_dir=\\192.168.8.1\Internal\var\cache
+set log_file=\\192.168.8.1\Internal\dev\robocopy_log.txt
 
 robocopy "%src_dir%" "%dst_dir%" /mov /s /xd /log:"%log_file%"
