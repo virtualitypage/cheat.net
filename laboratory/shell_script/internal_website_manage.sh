11e1f88d6ced736c96952d19af235eb203fdc51e
virtualitypage <ngd_apm@icloud.com>
2024-08-09 17:12:27 +0900

    Update internal_website_manage.sh

--- laboratory/shell_script/internal_website_manage.sh
+++ laboratory/shell_script/internal_website_manage.sh
@@ -6,22 +6,22 @@ internal="/tmp/mountd/disk1_part1/Internal/dev/!urandom"
 dir="/www"
 
 passwd_management () {
-  passphase=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1 | sort | uniq)
-  touch "$internal/passwd($passphase)"
-  file=$(grep "var validPassword =" "$main_file" | sed -e 's/  var validPassword = "//g' -e 's/"//g')
-  mv "$internal/passwd($file)" "$internal/passwd($passphase)" 2>/dev/null
-  mv "$dir/$file.html" "$dir/$passphase.html" 2>/dev/null
-  sed -i.bak "s/var validPassword = \".*/var validPassword = \"$passphase\"/g" "$main_file"
+  for i in {1..3}; do
+    passphase=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1 | uniq)
+    file_name=$(grep "var validPage_$i =" "$main_file" | sed -e "s/  var validPage_$i = \"//g" -e 's/"//g')
+    mv "$dir/$file_name.html" "$dir/$passphase.html" 2>/dev/null
+    sed -i.bak "s/var validPage_$i = \".*/var validPage_$i = \"$passphase\"/g" "$main_file"
+  done
   rm "$main_file.bak"
 }
 
 log_management () {
-  passphase=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1 | sort | uniq)
+  passphase=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1 | uniq)
   touch "$internal/log($passphase)"
-  file=$(grep "var validPasswordForLog =" "$main_file" | sed -e 's/  var validPasswordForLog = "//g' -e 's/"//g')
-  mv "$internal/log($file)" "$internal/log($passphase)" 2>/dev/null
-  mv "$dir/$file.html" "$dir/$passphase.html" 2>/dev/null
-  sed -i.bak "s/var validPasswordForLog = \".*/var validPasswordForLog = \"$passphase\"/g" "$main_file"
+  file_name=$(grep "var validLogPage =" "$main_file" | sed -e 's/  var validLogPage = "//g' -e 's/"//g')
+  mv "$internal/log($file_name)" "$internal/log($passphase)" 2>/dev/null
+  mv "$dir/$file_name.html" "$dir/$passphase.html" 2>/dev/null
+  sed -i.bak "s/var validLogPage = \".*/var validLogPage = \"$passphase\"/g" "$main_file"
   rm "$main_file.bak"
 }
 
