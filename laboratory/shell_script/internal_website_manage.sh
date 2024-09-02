2c38bcd3dab6eed042ab484e21f50902bf78dc53
virtualitypage <ngd_apm@icloud.com>
2024-09-02 21:18:06 +0900

    Update internal_website_manage.sh

--- laboratory/shell_script/internal_website_manage.sh
+++ laboratory/shell_script/internal_website_manage.sh
@@ -6,7 +6,7 @@ main_file="/www/js/admin.js"
 dir="/www"
 
 passwd_management () {
-  for i in {1..3}; do
+  for i in $(seq 1 3); do
     passphase=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z4-9' | fold -w 10 | head -n 1 | uniq)
     file_name=$(grep "var validPage_$i =" "$main_file" | sed -e "s/  var validPage_$i = \"//g" -e 's/"//g')
     if mv "$dir/${file_name}$i.html" "$dir/${passphase}$i.html" 2>/dev/null; then
@@ -21,8 +21,8 @@ passwd_management () {
 }
 
 log_archive () {
-  passphase=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1 | sort | uniq)
-  file_name=$(grep "var validLogPage = " "$main_file" | sed -e 's/  var validLogPage = "//g' -e 's/"//g')
+  passphase=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z4-9' | fold -w 10 | head -n 1 | sort | uniq)
+  file_name=$(grep "var validLogPage =" "$main_file" | sed -e 's/  var validLogPage = "//g' -e 's/"//g')
   if mv "$dir/${file_name}log.html" "$dir/${passphase}log.html" 2>/dev/null; then
     sed -i.bak "s/var validLogPage = \".*/var validLogPage = \"$passphase\"/g" "$main_file"
     rm "$main_file.bak"
