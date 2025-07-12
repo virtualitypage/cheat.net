0556f5119bd175a19dc7fb549da31b80c76e3bf2
virtualitypage <ngd_apm@icloud.com>
2025-07-13 00:28:33 +0900

    Update LINE Custom Notification.js

--- laboratory/google_apps_script/LINE Custom Notification.js
+++ laboratory/google_apps_script/LINE Custom Notification.js
@@ -7,7 +7,28 @@ function LineDeveloperMessage() {
   var channelAccessToken = "----CHANNEL_ACCESS_TOKEN-----";
   var groupId = "-----GROUP_ID-----";
 
-  // 以下、メッセージの内容を設定
+  var date = new Date();
+
+  var dayOfweek = '';
+  switch(date.getDay()){
+    case 0: dayOfweek = '日';
+    break;
+    case 1: dayOfweek = '月';
+    break;
+    case 2: dayOfweek = '火';
+    break;
+    case 3: dayOfweek = '水';
+    break;
+    case 4: dayOfweek = '木';
+    break;
+    case 5: dayOfweek = '金';
+    break;
+    case 6: dayOfweek = '土';
+    break;
+  }
+  var today = Utilities.formatDate(date, 'Asia/Tokyo', 'yyyy年MM月dd日({曜日}曜日)');
+  var today = today.replace('{曜日}', dayOfweek); // {曜日}を日本語の曜日に置換
+
   var headers = {
     "Authorization": "Bearer " + channelAccessToken,
     "Content-Type": "application/json"
@@ -17,18 +38,24 @@ function LineDeveloperMessage() {
     "type": "template",
     "altText": "【定期配信】告知代行 - 防犯カメラ関連",
     "template": {
-      "type": "confirm",
-      "text": "告知代行 - 防犯カメラ関連",
+      "type": "buttons",
+      "title": "告知代行",
+      "text": "防犯カメラ関連の対応を選択",
       "actions": [
         {
           "type": "message",
-          "label": "データ回収",
-          "text": "str.integer[0]"
+          "label": "動画データを回収した",
+          "text": "cmd:動画データ回収済み"
         },
         {
           "type": "message",
-          "label": "電池交換",
-          "text": "str.integer[1]"
+          "label": "動画データを削除した",
+          "text": "cmd:動画データ削除済み"
+        },
+        {
+          "type": "message",
+          "label": "電池交換を行う",
+          "text": "早速防犯カメラの電池交換を行う、後に続け！"
         }
       ]
     }
@@ -49,7 +76,7 @@ function LineDeveloperMessage() {
 
 function initializeTrigger() { // 通知用のトリガーを定期的に作成する
   ScriptApp.newTrigger('initializeTrigger').timeBased().onWeekDay(ScriptApp.WeekDay.SUNDAY).atHour(0).create();
-  console.log("initializeTrigger(): 初期化トリガーを作成しました");
+  Logger.log("initializeTrigger(): 初期化トリガーを作成しました");
 }
 
 function createTrigger() {
@@ -62,5 +89,5 @@ function createTrigger() {
   ScriptApp.newTrigger("LineDeveloperMessage").timeBased().onWeekDay(ScriptApp.WeekDay.MONDAY).atHour(0).create();
   ScriptApp.newTrigger("LineDeveloperMessage").timeBased().onWeekDay(ScriptApp.WeekDay.WEDNESDAY).atHour(0).create();
   ScriptApp.newTrigger("LineDeveloperMessage").timeBased().onWeekDay(ScriptApp.WeekDay.FRIDAY).atHour(0).create();
-  console.log("createTrigger(): LineDeveloperMessage実行トリガーを作成しました");
+  Logger.log("createTrigger(): LineDeveloperMessage 実行トリガーを作成しました");
 }
